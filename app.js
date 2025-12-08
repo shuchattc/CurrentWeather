dbd30c05c70d535c7a30ffbef9f2872f13f0fc5b
Tyler Shuchat <tyshuchat@gmail.com>
2025-12-08 16:54:14 -0500

    Added dotenv npm package

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const search = document.getElementById("search");
 const city = document.getElementById("city");
 const c = document.getElementById("c");
 const form = document.querySelector('#searchForm');
+require('dotenv').config();
 const apiKey = process.env.API_KEY;
 
 form.addEventListener('submit', async function (e) {
